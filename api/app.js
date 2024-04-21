d3212b7eac708a9f095897d5e19e46e2711c09be
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2024-04-21 14:35:29 -0300

    Create fist endpoint

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import multer from "multer";
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
@@ -8,3 +9,20 @@ app.listen(app.get("port"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype !== 'application/json') {
+      return cb(new Error('Solo se permiten archivos JSON'));
+    }
+    cb(null, true);
+  };
+  
+const upload = multer({storage: multer.memoryStorage(),fileFilter})
+const read = (req, res) => {
+    let content = req.body
+    if(req.file){
+        const { buffer } = req.file;
+        content = JSON.parse(buffer.toString('utf8')); 
+    }
+    return res.json(content)
+}
+app.post("/analize",upload.single('file'),read)
